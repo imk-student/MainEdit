15b54fecebeec0dad7e8365558b1396b0c68d282
A M <Alcyonespace@gmail.com>
2024-04-12 21:02:01 +0300

    5

--- client/src/TextEditor.tsx
+++ client/src/TextEditor.tsx
@@ -4,6 +4,7 @@ import Quill from "quill";
 import {io} from 'socket.io-client'
 import {Socket} from 'socket.io-client'
 import {DefaultEventsMap} from "@socket.io/component-emitter"
+import { useParams } from 'react-router-dom';
 
    
 const SAVE_INTERVAL_MS = 2000
@@ -22,8 +23,10 @@ const TOOLBAR_OPTIONS=[
 ]
 
 export default function TextEditor(){
+    const {id: documentId} = useParams()
     const [socket, setSocket] = useState<Socket<DefaultEventsMap, DefaultEventsMap> | null>(null)
     const [quill, setQuill] = useState<any>(null)
+    console.log(documentId)
 
     useEffect(()=>{
         
@@ -35,7 +38,26 @@ export default function TextEditor(){
 
     }, [])
 
-   
+    useEffect(() => {
+        if (socket == null || quill == null) return
+        socket.once("load-document", document =>{
+            quill.setContents(document)
+            quill.enable()
+        })
+        socket.emit('get-document', documentId)
+    },[socket, quill, documentId])
+
+useEffect(() => {
+    if (socket == null || quill == null) return
+    const interval = setInterval(()=>{
+        socket.emit('save-document', quill.getContents())
+
+    }, SAVE_INTERVAL_MS)
+
+    return () => {
+        clearInterval(interval)
+    }
+}, [socket, quill])
 
    useEffect(() => {
         if (socket == null || quill == null) return
@@ -50,12 +72,45 @@ export default function TextEditor(){
         }
     }, [socket, quill])
 
+
+    useEffect(() => {
+
+        if (socket == null || quill == null) return
+
+        const handler = (delta:Object) => {
+            quill.updateContents(delta)
+        }
+        socket.on('receive-change', handler)
+
+        return () => {
+            socket.off('receive-change', handler)
+        }
+    }, [socket, quill])
+
+
+    useEffect(() => {
+
+        if (socket == null || quill == null) return
+
+        const handler = (delta:Object, oldDelta:Object, source:string) => {
+            if (source !== 'user') return
+            socket?.emit("send-changes", delta)
+        }
+        quill.on('text-change', handler)
+
+        return () => {
+            quill.off('text-change', handler)
+        }
+    }, [socket, quill])
+
     const wrapperRef = useCallback((wrapper:HTMLDivElement)=> {
         if (wrapper==null) return
         wrapper.innerHTML = ""
         const editor = document.createElement('div')
         wrapper.append(editor)
         const q = new Quill(editor, {theme: 'snow', modules: {toolbar: TOOLBAR_OPTIONS}})
+        q.disable()
+        q.setText('Loading...')
         setQuill(q)
     }, [])
 

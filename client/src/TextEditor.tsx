1bf2019f54b9a5bd00087d1b58b1e3b9ab4abd5c
Макшанова Алла Андреевна <Makshanova_AA@msk.aep.local>
2024-04-12 15:57:56 +0300

    12334

--- client/src/TextEditor.tsx
+++ client/src/TextEditor.tsx
@@ -2,11 +2,12 @@ import React, { SetStateAction, useCallback, useEffect, useState} from 'react';
 import "quill/dist/quill.snow.css"
 import Quill from "quill";
 import {io} from 'socket.io-client'
-
-
+import {Socket} from 'socket.io-client'
+import {DefaultEventsMap} from "@socket.io/component-emitter"
 
    
 const SAVE_INTERVAL_MS = 2000
+
 
 const TOOLBAR_OPTIONS=[
     [{header:[1,2,3,4,5,6,false]}],
@@ -21,26 +22,41 @@ const TOOLBAR_OPTIONS=[
 ]
 
 export default function TextEditor(){
-    const [socket, setSocket] = useState(null)
-    const [quill, setQuill] = useState()
+    const [socket, setSocket] = useState<Socket<DefaultEventsMap, DefaultEventsMap> | null>(null)
+    const [quill, setQuill] = useState<any>(null)
 
     useEffect(()=>{
         
-        const s = io("http://localhost:3001") 
+    const s = io("http://localhost:3001")
         setSocket(s)
-
         return () => {
             s.disconnect()
         }
+
     }, [])
+
+   
+
+   useEffect(() => {
+        if (socket == null || quill == null) return
+         const handler = (delta:Object, oldDelta:Object, source:string) => {
+            if (source !== 'user') return 
+            socket.emit("send-changes", delta)
+        }
+        quill.on('text-change',handler)
+
+        return() => {
+            quill.off('text-change', handler)
+        }
+    }, [socket, quill])
 
     const wrapperRef = useCallback((wrapper:HTMLDivElement)=> {
         if (wrapper==null) return
         wrapper.innerHTML = ""
         const editor = document.createElement('div')
         wrapper.append(editor)
-        new Quill(editor, {theme: 'snow', modules: {toolbar: TOOLBAR_OPTIONS}})
-
+        const q = new Quill(editor, {theme: 'snow', modules: {toolbar: TOOLBAR_OPTIONS}})
+        setQuill(q)
     }, [])
 
     return(

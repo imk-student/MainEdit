d9731a99104e9afff9f4a0e7302070c750f6b7c6
A M <Alcyonespace@gmail.com>
2024-04-16 22:03:23 +0300

    css

--- client/declaration.d.ts
+++ client/declaration.d.ts
@@ -1,6 +1,4 @@
-
+declare module "*.module.css";
 declare module "*.module.scss";
-declare module '*.scss' {
-    const content: Record<string, string>;
-    export default content;
-}
+declare module '*.scss';
+declare module '*.css';
